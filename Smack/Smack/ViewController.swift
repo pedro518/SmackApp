b7202a133dfd2f011cbc93ccfc946e1d055f2be0
Pedro Antonio Ruiz Cuesta <pruiz@Pedros-iMac.local>
2019-05-22 10:15:32 -0700

    Initial commit

---
+++ Smack/Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Pedro Antonio Ruiz Cuesta on 22/05/2019.
+//  Copyright © 2019 Pedro Antonio Ruiz Cuesta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
